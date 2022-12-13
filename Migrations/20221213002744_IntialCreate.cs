1e56f6227d9a24e35e10f0f8d2510b38f45f7af8
Jackson-Lyn <jason.lyn1@dcmail.ca>
2022-12-12 20:51:14 -0500

    Add project files.

---
+++ Migrations/20221213002744_IntialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab5NETD.Migrations
+{
+    public partial class IntialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    CardID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EstimatedMarketPrice = table.Column<int>(type: "int", nullable: false),
+                    PurchasePrice = table.Column<int>(type: "int", nullable: false),
+                    Info = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.CardID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
---
+++ Models/CardContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab5NETD.Models
+{
+    public class CardContext : DbContext
+    {
+        //Constructor
+
+        public CardContext(DbContextOptions<CardContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Card> Cards { get; set; }
+    }
+}
